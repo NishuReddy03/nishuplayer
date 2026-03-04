e5b6a5780df39e4a708b9ef7005065cca856ac89
NishuReddy03 <nishureddy0@gmail.com>
2026-03-04 21:25:47 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'NishuPlayer',
+        short_name: 'Nishu',
+        description: 'Modern, High-Quality Music Player',
+        theme_color: '#121212',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
+})
